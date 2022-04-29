43906991943229222c13c12c492a4a0b61aa927d
Carey Auguste <40592376@live.napier.ac.uk>
2022-04-28 22:42:43 -0400

    worked on gallery

--- saver.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Datastore = require('nedb');
-
-const app = express();
-app.listen(8080, () => console.log('listening at 8080'));
-
-app.use(express.static('CAREYKVA_QUIZ'));
-app.use(express.json({ limit: '1mb' }));
